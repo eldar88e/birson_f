3173f0c517454e605760ec2ec00406f2b343056e
Eldar Eminov <em88el@gmail.com>
2025-12-31 22:27:05 +0300

    Refactor

--- src/components/users/UserList.tsx
+++ src/components/users/UserList.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useMemo, useState } from "react";
 import TableDropdown from "../common/TableDropdown";
 import { apiClient } from "../../api/client";
 import Pagination from "../../shared/ui/Pages";
+import SvgIcon from "../../shared/ui/SvgIcon";
 
 interface User {
   id: string;
@@ -200,21 +201,7 @@ const UserList: React.FC = () => {
           <div className="hidden flex-col gap-3 sm:flex sm:flex-row sm:items-center">
             <div className="relative">
               <span className="absolute top-1/2 left-4 -translate-y-1/2 text-gray-500 dark:text-gray-400">
-                <svg
-                  className="fill-current"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M3.04199 9.37363C3.04199 5.87693 5.87735 3.04199 9.37533 3.04199C12.8733 3.04199 15.7087 5.87693 15.7087 9.37363C15.7087 12.8703 12.8733 15.7053 9.37533 15.7053C5.87735 15.7053 3.04199 12.8703 3.04199 9.37363ZM9.37533 1.54199C5.04926 1.54199 1.54199 5.04817 1.54199 9.37363C1.54199 13.6991 5.04926 17.2053 9.37533 17.2053C11.2676 17.2053 13.0032 16.5344 14.3572 15.4176L17.1773 18.238C17.4702 18.5309 17.945 18.5309 18.2379 18.238C18.5308 17.9451 18.5309 17.4703 18.238 17.1773L15.4182 14.3573C16.5367 13.0033 17.2087 11.2669 17.2087 9.37363C17.2087 5.04817 13.7014 1.54199 9.37533 1.54199Z"
-                    fill=""
-                  />
-                </svg>
+                <SvgIcon name="search" />
               </span>
               <input
                 type="text"
@@ -224,65 +211,6 @@ const UserList: React.FC = () => {
                 className="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent py-2.5 pr-4 pl-11 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden xl:w-[300px] dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
               />
             </div>
-            {/*<div className="relative filter-dropdown">*/}
-            {/*  <button*/}
-            {/*    className="shadow-theme-xs flex h-11 w-full items-center justify-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-sm font-medium text-gray-700 sm:w-auto sm:min-w-[100px] dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400"*/}
-            {/*    onClick={() => setShowFilter(!showFilter)}*/}
-            {/*    type="button"*/}
-            {/*  >*/}
-            {/*    <svg*/}
-            {/*      xmlns="http://www.w3.org/2000/svg"*/}
-            {/*      width="20"*/}
-            {/*      height="20"*/}
-            {/*      viewBox="0 0 20 20"*/}
-            {/*      fill="none"*/}
-            {/*    >*/}
-            {/*      <path*/}
-            {/*        d="M14.6537 5.90414C14.6537 4.48433 13.5027 3.33331 12.0829 3.33331C10.6631 3.33331 9.51206 4.48433 9.51204 5.90415M14.6537 5.90414C14.6537 7.32398 13.5027 8.47498 12.0829 8.47498C10.663 8.47498 9.51204 7.32398 9.51204 5.90415M14.6537 5.90414L17.7087 5.90411M9.51204 5.90415L2.29199 5.90411M5.34694 14.0958C5.34694 12.676 6.49794 11.525 7.91777 11.525C9.33761 11.525 10.4886 12.676 10.4886 14.0958M5.34694 14.0958C5.34694 15.5156 6.49794 16.6666 7.91778 16.6666C9.33761 16.6666 10.4886 15.5156 10.4886 14.0958M5.34694 14.0958L2.29199 14.0958M10.4886 14.0958L17.7087 14.0958"*/}
-            {/*        stroke="currentColor"*/}
-            {/*        strokeWidth="1.5"*/}
-            {/*        strokeLinecap="round"*/}
-            {/*        strokeLinejoin="round"*/}
-            {/*      />*/}
-            {/*    </svg>*/}
-            {/*    Filter*/}
-            {/*  </button>*/}
-            {/*  {showFilter && (*/}
-            {/*    <div className="absolute right-0 z-10 mt-2 w-56 rounded-lg border border-gray-200 bg-white p-4 shadow-lg dark:border-gray-700 dark:bg-gray-800">*/}
-            {/*      <div className="mb-5">*/}
-            {/*        <label className="mb-2 block text-xs font-medium text-gray-700 dark:text-gray-300">*/}
-            {/*          Роль*/}
-            {/*        </label>*/}
-            {/*        <input*/}
-            {/*          type="text"*/}
-            {/*          name="role"*/}
-            {/*          value={roleFilter}*/}
-            {/*          onChange={(e) => setRoleFilter(e.target.value)}*/}
-            {/*          className="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-10 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"*/}
-            {/*          placeholder="Поиск по роли..."*/}
-            {/*        />*/}
-            {/*      </div>*/}
-            {/*      <div className="flex gap-2">*/}
-            {/*        <button*/}
-            {/*          className="bg-brand-500 hover:bg-brand-600 h-10 flex-1 rounded-lg px-3 py-2 text-sm font-medium text-white"*/}
-            {/*          onClick={() => setShowFilter(false)}*/}
-            {/*        >*/}
-            {/*          Применить*/}
-            {/*        </button>*/}
-            {/*        <button*/}
-            {/*          className="bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 h-10 flex-1 rounded-lg px-3 py-2 text-sm font-medium text-gray-700 dark:text-gray-300"*/}
-            {/*          onClick={() => {*/}
-            {/*            setRoleFilter("");*/}
-            {/*            setSelectedRole("All");*/}
-            {/*            setShowFilter(false);*/}
-            {/*          }}*/}
-            {/*        >*/}
-            {/*          Сбросить*/}
-            {/*        </button>*/}
-            {/*      </div>*/}
-            {/*    </div>*/}
-            {/*  )}*/}
-            {/*</div>*/}
           </div>
         </div>
       </div>
@@ -309,21 +237,7 @@ const UserList: React.FC = () => {
                           }`}
                         >
                           <span className={selectAll ? "" : "opacity-0"}>
-                            <svg
-                              width="12"
-                              height="12"
-                              viewBox="0 0 12 12"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                d="M10 3L4.5 8.5L2 6"
-                                stroke="white"
-                                strokeWidth="1.6666"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              />
-                            </svg>
+                            <SvgIcon name="check" />
                           </span>
                         </span>
                       </span>
@@ -466,21 +380,7 @@ const UserList: React.FC = () => {
                               selected.includes(user.id) ? "" : "opacity-0"
                             }
                           >
-                            <svg
-                              width="12"
-                              height="12"
-                              viewBox="0 0 12 12"
-                              fill="none"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                d="M10 3L4.5 8.5L2 6"
-                                stroke="white"
-                                strokeWidth="1.6666"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              />
-                            </svg>
+                            <SvgIcon name="check" />
                           </span>
                         </span>
                       </span>
@@ -517,21 +417,7 @@ const UserList: React.FC = () => {
                     <TableDropdown
                       dropdownButton={
                         <button className="text-gray-500 dark:text-gray-400 ">
-                          <svg
-                            className="fill-current"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              clipRule="evenodd"
-                              d="M5.99902 10.245C6.96552 10.245 7.74902 11.0285 7.74902 11.995V12.005C7.74902 12.9715 6.96552 13.755 5.99902 13.755C5.03253 13.755 4.24902 12.9715 4.24902 12.005V11.995C4.24902 11.0285 5.03253 10.245 5.99902 10.245ZM17.999 10.245C18.9655 10.245 19.749 11.0285 19.749 11.995V12.005C19.749 12.9715 18.9655 13.755 17.999 13.755C17.0325 13.755 16.249 12.9715 16.249 12.005V11.995C16.249 11.0285 17.0325 10.245 17.999 10.245ZM13.749 11.995C13.749 11.0285 12.9655 10.245 11.999 10.245C11.0325 10.245 10.249 11.0285 10.249 11.995V12.005C10.249 12.9715 11.0325 13.755 11.999 13.755C12.9655 13.755 13.749 12.9715 13.749 12.005V11.995Z"
-                              fill=""
-                            />
-                          </svg>
+                          <SvgIcon name="more-horizontal" />
                         </button>
                       }
                       dropdownContent={
@@ -569,75 +455,6 @@ const UserList: React.FC = () => {
             </span>
           </span>
         </div>
-        {/*<div className="flex items-center dark:bg-white/[0.03] sm:bg-transparent dark:sm:bg-transparent justify-between gap-2 bg-gray-50 p-4 rounded-lg w-full sm:w-auto sm:justify-normal">*/}
-        {/*  <button*/}
-        {/*    onClick={handlePrevPage}*/}
-        {/*    disabled={currentPage === 1}*/}
-        {/*    className="shadow-theme-xs flex items-center gap-2 rounded-lg border border-gray-300 bg-white p-2 text-gray-700 hover:bg-gray-50 hover:text-gray-800 disabled:cursor-not-allowed disabled:opacity-50 sm:p-2.5 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"*/}
-        {/*  >*/}
-        {/*    <span>*/}
-        {/*      <svg*/}
-        {/*        className="fill-current"*/}
-        {/*        width="20"*/}
-        {/*        height="20"*/}
-        {/*        viewBox="0 0 20 20"*/}
-        {/*        fill="none"*/}
-        {/*        xmlns="http://www.w3.org/2000/svg"*/}
-        {/*      >*/}
-        {/*        <path*/}
-        {/*          fillRule="evenodd"*/}
-        {/*          clipRule="evenodd"*/}
-        {/*          d="M2.58203 9.99868C2.58174 10.1909 2.6549 10.3833 2.80152 10.53L7.79818 15.5301C8.09097 15.8231 8.56584 15.8233 8.85883 15.5305C9.15183 15.2377 9.152 14.7629 8.85921 14.4699L5.13911 10.7472L16.6665 10.7472C17.0807 10.7472 17.4165 10.4114 17.4165 9.99715C17.4165 9.58294 17.0807 9.24715 16.6665 9.24715L5.14456 9.24715L8.85919 5.53016C9.15199 5.23717 9.15184 4.7623 8.85885 4.4695C8.56587 4.1767 8.09099 4.17685 7.79819 4.46984L2.84069 9.43049C2.68224 9.568 2.58203 9.77087 2.58203 9.99715C2.58203 9.99766 2.58203 9.99817 2.58203 9.99868Z"*/}
-        {/*        />*/}
-        {/*      </svg>*/}
-        {/*    </span>*/}
-        {/*  </button>*/}
-        {/*  <span className="block text-sm font-medium text-gray-700 sm:hidden dark:text-gray-400">*/}
-        {/*    Page {currentPage} of {totalPages}*/}
-        {/*  </span>*/}
-        {/*  <ul className="hidden items-center gap-0.5 sm:flex">*/}
-        {/*    {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (*/}
-        {/*      <li key={page}>*/}
-        {/*        <a*/}
-        {/*          href="#"*/}
-        {/*          onClick={(e) => {*/}
-        {/*            e.preventDefault();*/}
-        {/*            handleGoToPage(page);*/}
-        {/*          }}*/}
-        {/*          className={`flex h-10 w-10 items-center justify-center rounded-lg text-sm font-medium ${*/}
-        {/*            currentPage === page*/}
-        {/*              ? "bg-brand-500 hover:bg-brand-500 text-white hover:text-white"*/}
-        {/*              : "text-gray-700 hover:bg-brand-500 hover:text-white dark:text-gray-400 dark:hover:text-white"*/}
-        {/*          }`}*/}
-        {/*        >*/}
-        {/*          {page}*/}
-        {/*        </a>*/}
-        {/*      </li>*/}
-        {/*    ))}*/}
-        {/*  </ul>*/}
-        {/*  <button*/}
-        {/*    onClick={handleNextPage}*/}
-        {/*    disabled={currentPage === totalPages}*/}
-        {/*    className="shadow-theme-xs flex items-center gap-2 rounded-lg border border-gray-300 bg-white p-2 text-gray-700 hover:bg-gray-50 hover:text-gray-800 disabled:cursor-not-allowed disabled:opacity-50 sm:p-2.5 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"*/}
-        {/*  >*/}
-        {/*    <span>*/}
-        {/*      <svg*/}
-        {/*        className="fill-current"*/}
-        {/*        width="20"*/}
-        {/*        height="20"*/}
-        {/*        viewBox="0 0 20 20"*/}
-        {/*        fill="none"*/}
-        {/*        xmlns="http://www.w3.org/2000/svg"*/}
-        {/*      >*/}
-        {/*        <path*/}
-        {/*          fillRule="evenodd"*/}
-        {/*          clipRule="evenodd"*/}
-        {/*          d="M17.4165 9.9986C17.4168 10.1909 17.3437 10.3832 17.197 10.53L12.2004 15.5301C11.9076 15.8231 11.4327 15.8233 11.1397 15.5305C10.8467 15.2377 10.8465 14.7629 11.1393 14.4699L14.8594 10.7472L3.33203 10.7472C2.91782 10.7472 2.58203 10.4114 2.58203 9.99715C2.58203 9.58294 2.91782 9.24715 3.33203 9.24715L14.854 9.24715L11.1393 5.53016C10.8465 5.23717 10.8467 4.7623 11.1397 4.4695C11.4327 4.1767 11.9075 4.17685 12.2003 4.46984L17.1578 9.43049C17.3163 9.568 17.4165 9.77087 17.4165 9.99715C17.4165 9.99763 17.4165 9.99812 17.4165 9.9986Z"*/}
-        {/*        />*/}
-        {/*      </svg>*/}
-        {/*    </span>*/}
-        {/*  </button>*/}
-        {/*</div>*/}
         <Pagination
           currentPage={currentPage}
           totalPages={totalPages}
