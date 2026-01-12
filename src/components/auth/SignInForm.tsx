ea77ac6638005bb5cf541ed0580b14fe04e3c5e0
Eldar Eminov <em88el@gmail.com>
2026-01-12 19:23:30 +0300

    Refactor

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -7,6 +7,7 @@ import Checkbox from "../form/input/Checkbox";
 import Button from "../ui/button/Button";
 import { authService } from "../../api/auth";
 import { ApiError } from "../../api/client";
+import SvgIcon from "../../shared/ui/SvgIcon";
 
 export default function SignInForm() {
   const navigate = useNavigate();
@@ -52,6 +53,9 @@ export default function SignInForm() {
     <div className="flex flex-col flex-1">
       <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
         <div>
+          <div className="mb-8 sm:mb-12 flex justify-center text-black dark:text-white">
+            <SvgIcon name="logo" width={240} height={60} />
+          </div>
           <div className="mb-5 sm:mb-8">
             <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
               Вход в систему
