d9dd1f299625deaffb7c9fd2c43dfc19c8e62cd3
Eldar Eminov <em88el@gmail.com>
2026-01-12 14:36:40 +0300

    Refactor

--- src/components/UserProfile/UserMetaCard.tsx
+++ src/components/UserProfile/UserMetaCard.tsx
@@ -12,7 +12,8 @@ export default function UserMetaCard({ user }: UserMetaCardProps) {
       <div className="p-5 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6">
         <div className="flex flex-col gap-5 xl:flex-row xl:items-center xl:justify-between">
           <div className="flex flex-col items-center w-full gap-6 xl:flex-row">
-            <AvatarText name={user.full_name} size={20} classText="text-3xl" />
+            <span className="h-17 w-17 hidden"></span>
+            <AvatarText name={user.full_name} size={17} classText="text-3xl" />
             <div className="order-3 xl:order-2">
               <h4 className="mb-2 text-lg font-semibold text-center text-gray-800 dark:text-white/90 xl:text-left">
                 {user.full_name}
